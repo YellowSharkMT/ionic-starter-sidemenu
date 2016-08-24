be15a8aae7b27b3b1c27989abd955a09a17a52ad
The Yellow Shark <bowdowntozappa@yahoo.com>
2016-08-24 10:47:47 -0600

    2nd part of demonstrating keyboard whitespace bug: call ionic.Platform.fullScreen

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
+    ionic.Platform.fullScreen(true, true);
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if (window.cordova && window.cordova.plugins.Keyboard) {
